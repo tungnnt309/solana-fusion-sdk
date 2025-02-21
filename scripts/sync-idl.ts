63c7f70515bb6900929430f27d976e899b80cafe
Vladimir Borovik <v.borovik@1inch.io>
2025-02-21 14:49:30 +0400

    feat: sync IDL script

---
+++ scripts/sync-idl.ts
@@ -0,0 +1,19 @@
+import {execSync} from 'child_process'
+import path from 'node:path'
+
+function main(): void {
+    const root = path.join(__dirname, '..')
+    const contractDir = path.join(__dirname, '../contracts/fusion-contract')
+
+    execSync('yarn && yarn build', {
+        cwd: contractDir,
+        stdio: 'inherit'
+    })
+
+    execSync('cp ./contracts/fusion-contract/target/idl/* ./src/idl', {
+        cwd: root,
+        stdio: 'inherit'
+    })
+}
+
+main()
