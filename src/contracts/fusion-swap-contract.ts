4753bdd73a7f51dd095a9738a6cbf85d992c69bc
Kirill Kuznetcov <krboktv@gmail.com>
2025-04-28 18:10:43 +0400

    fix: isWritable for maker_receiver

--- src/contracts/fusion-swap-contract.ts
+++ src/contracts/fusion-swap-contract.ts
@@ -427,7 +427,7 @@ export class FusionSwapContract {
                 {
                     // 4. maker_receiver
                     pubkey: order.receiver,
-                    isWritable: order.dstAssetIsNative,
+                    isWritable: order.srcAssetIsNative,
                     isSigner: false
                 },
                 {
